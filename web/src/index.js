e59f018a32fb62c4afd2b08e9bdf83d2e7869099
shiwu <152254394+shuwu-ui@users.noreply.github.com>
2025-06-05 01:42:41 -0400

    refactor: overhaul frontend layout

--- web/src/index.js
+++ web/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import HeaderBar from './components/HeaderBar';
 import 'semantic-ui-offline/semantic.min.css';
 import './index.css';
 import { UserProvider } from './context/User';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { StatusProvider } from './context/Status';
-import { Layout } from '@douyinfe/semi-ui';
-import SiderBar from './components/SiderBar';
 import { ThemeProvider } from './context/Theme';
-import FooterBar from './components/Footer';
 import { StyleProvider } from './context/Style/index.js';
 import PageLayout from './components/PageLayout.js';
 import './i18n/i18n.js';
@@ -20,7 +16,6 @@ import './i18n/i18n.js';
 // initialization
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const { Sider, Content, Header, Footer } = Layout;
 root.render(
   <React.StrictMode>
     <StatusProvider>
